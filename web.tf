31ff6a8626fd0aa3af159b7c7fc7cfa8c98300a6
qafro1 <gofokere@gmail.com>
2017-03-28 21:03:39 +0100

    terraform beta

---
+++ web.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "${var.region}"
+
+}
+
+module "vpc" {
+  source        = "./vpc"
+  name          = "web"
+  cidr          = "10.0.0.0/16"
+  public_subnet = "10.0.1.0/24"
+}
+
+resource "aws_instance" "web" {
+
+}
